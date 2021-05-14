b9a593cdce07ecd0864372bd666a8dc7f4513d22
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-05-14 13:49:16 -0500

    Update __init__.py

--- try00/__init__.py
+++ try00/__init__.py
@@ -9,15 +9,3 @@ def exists():
 def compiles():
   """no syntax errors in tracy.py"""
   check50.run("python -m py_compile tracy.py").exit(0)
-  
-@check50.check(compiles)
-def check_circle():
-  """circle radius is 35"""
-  datafile = file('tracy.py')
-  found = False
-  for line in datafile:
-    if "circle(35)" in line:
-      found = True
-      break
-  if not found:
-    raise check50.Mismatch("","")

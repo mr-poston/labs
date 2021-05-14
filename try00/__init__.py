8603cc5ca56cf5188cfdfed8ea928e66ddfadffd
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-05-14 14:38:19 -0500

    Update __init__.py

--- try00/__init__.py
+++ try00/__init__.py
@@ -9,3 +9,14 @@ def exists():
 def compiles():
   """no syntax errors in tracy.py"""
   check50.run("python -m py_compile tracy.py").exit(0)
+
+@check50.check(compiles)
+def check_circles():
+  """code creates 5 circles with radii of 35"""
+  datafile = file('tracy.py')
+  count = 0
+  for line in datafile:
+    if line == "circle(35)":
+      count += 1
+  if count != 5:
+    raise check50.Failure("5 circles with radii of 35 not found")

8174baf0169bfd6ddcbd92c8d0c0e1e466b34f7b
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-05-14 15:17:41 -0500

    Update __init__.py

--- try00/__init__.py
+++ try00/__init__.py
@@ -5,10 +5,12 @@ def exists():
   """tracy.py exists."""
   check50.exists("tracy.py")
   
+"""
 @check50.check(exists)
 def compiles():
   """no syntax errors in tracy.py"""
   check50.run("python -m py_compile tracy.py").exit(0)
+"""
 
 @check50.check()
 def check_circles():

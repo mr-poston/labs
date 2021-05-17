5e14c13621c74580998f41bf944a8ded3fcb42d4
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-05-17 08:47:50 -0500

    Add files via upload

--- try00/__init__.py
+++ try00/__init__.py
@@ -2,49 +2,49 @@ import check50
 
 @check50.check()
 def exists():
-  """tracy.py exists."""
-  check50.exists("tracy.py")
-  
+    """tracy.py exists"""
+    check50.exists("tracy.py")
+
 @check50.check(exists)
-  def check_draw():
-    """draws 5 circles spaced by 40"""
+def check_draw():
+    """drawing matches description"""
     grader = open("grader.py", "w")
     tracy = open("tracy.py", "r")
     for line in tracy:
-      if line.strip() == "circle(35)":
-        grader.write("print(circle(35))")
-      elif line.strip() == "forward(40)":
-        grader.write("print(forward(40))")
-      else:
-        grader.write(line)
+        if line.strip() == "circle(35)":
+            grader.write(line.replace("circle(35)", "print('circle(35)')"))
+        elif line.strip() == "forward(40)":
+            grader.write(line.replace("forward(40)", "print('forward(40)')"))
+        else:
+            grader.write(line)
     grader.close()
     tracy.close()
     check50.run("python grader.py").stdout("circle(35)\nforward(40)" + \
-                                           "circle(35)\nforward(40)" + \
-                                           "circle(35)\nforward(40)" + \
-                                           "circle(35)\nforward(40)" + \
-                                           "circle(35)\nforward(40)", regex=False).exit(0)
+    "circle(35)\nforward(40)" + \
+    "circle(35)\nforward(40)" + \
+    "circle(35)\nforward(40)" + \
+    "circle(35)\nforward(40)", regex=False).exit(0)
 
-@check50.check()
+@check50.check(exists)
 def check_circles():
-  """code creates 5 circles with radii of 35"""
-  datafile = open("tracy.py", "r")
-  count = 0
-  for line in datafile:
-    if line.strip() == "circle(35)":
-      count += 1
-  if count != 5:
-    raise check50.Failure("5 circles with radii of 35 not found")
+    """code creates 5 circles with radii of 35"""
+    tracy = open("tracy.py", "r")
+    count = 0
+    for line in tracy:
+        if line.strip() == "circle(35)":
+            count += 1
+    if count != 5:
+        raise check50.Failure("5 circles with radii of 35 not found")
 
-@check50.check()
+@check50.check(exists)
 def check_forward():
-  """Tracy moves forward 40 after every circle"""
-  datafile = open("tracy.py", "r")
-  result = ""
-  for line in datafile:
-    if line.strip() == "circle(35)":
-      result += "c"
-    if line.strip() == "forward(40)":
-      result += "f"
-  if result != "cfcfcfcfcf":
-    raise check50.Failure("Tracy did not move forward 40 after each circle")
+    """Tracy moves forward 40 after every circle"""
+    tracy = open("tracy.py", "r")
+    result = ""
+    for line in tracy:
+        if line.strip() == "circle(35)":
+            result += "c"
+        if line.strip() == "forward(40)":
+            result += "f"
+    if result != "cfcfcfcfcf":
+        raise check50.Failure("Tracy did not move forward 40 after each circle")

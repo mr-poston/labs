fa8bc12cbe48ac5bc07c13dde67234318b807fbd
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-05-18 12:47:07 -0500

    Add files via upload

---
+++ 19_dartboard/admin/__init__.py
@@ -0,0 +1,60 @@
+import check50
+
+@check50.check()
+def exists():
+    """tracy.py exists"""
+    check50.exists("tracy.py")
+
+@check50.check(exists)
+def compiles():
+    """No syntax errors in tracy.py"""
+    check50.run("python3 -m py_compile tracy.py").exit(0)
+
+@check50.check(compiles)
+def check_draw():
+    """Drawing looks correct"""
+    turtle = ["from __setup import *", "begin()", "end()"]
+    grader = open("grader.py", "w")
+    tracy = open("tracy.py", "r")
+    for line in tracy:
+        if line.strip() in turtle:
+            grader.write("#" + line)
+        elif "radius" in line and "+" in line:
+            grader.write(line + "\n\tprint(radius)")
+        elif "radius" in line and "circle" not in line:
+            grader.write(line)
+        elif "def " not in line:
+            grader.write("#" + line)
+        elif "for " not in line:
+            grader.write("#" + line)
+        else:
+            grader.write(line)
+    grader.close()
+    tracy.close()
+    check50.run("python3 grader.py").stdout("25\n50\n75\n100", regex=False).exit(0)
+
+@check50.check(compiles)
+def check_function():
+    """You have a function in your code"""
+    turtle = ["from __setup import *", "begin()", "end()"]
+    grader = open("grader.py", "w")
+    tracy = open("tracy.py", "r")
+    for line in tracy:
+        if "def " in line:
+            grader.write('print("' + line.strip() + '")')
+    grader.close()
+    tracy.close()
+    check50.run("python3 grader.py").stdout("^def [a-z].*\(\):$", regex=True).exit(0)
+
+@check50.check(compiles)
+def check_for():
+    """You have a for loop in your code"""
+    turtle = ["from __setup import *", "begin()", "end()"]
+    grader = open("grader.py", "w")
+    tracy = open("tracy.py", "r")
+    for line in tracy:
+        if "for " in line:
+            grader.write('print("' + line.strip() + '")')
+    grader.close()
+    tracy.close()
+    check50.run("python3 grader.py").stdout("^for [a-z].*\(4\):$", regex=True).exit(0)
---
+++ 19_dartboard/main.py
@@ -0,0 +1,12 @@
+from setup.setup import *
+begin()
+# Your code goes below here
+# |    |    |    |    |    |
+# v    v    v    v    v    v
+
+
+
+# ^    ^    ^    ^    ^    ^
+# |    |    |    |    |    |
+# Your code goes above here
+end()

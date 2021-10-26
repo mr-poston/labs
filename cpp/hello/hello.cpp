aac3716c1cf2c60a547c69650e7a6f12c4a66972
mr-poston <60262786+mr-poston@users.noreply.github.com>
2021-10-26 15:23:02 -0500

    Update hello.cpp

--- cpp/hello/hello.cpp
+++ cpp/hello/hello.cpp
@@ -4,7 +4,26 @@ using namespace std;
 
 int main()
 {
-  cout << "Hello World!";
-  
-  return 0;
+    // To read an entire line, you use the getline command.
+    cout << "Please enter your full name: ";
+    
+    // Create a string variable and read in from the user
+    string fullName;
+    getline(cin, fullName);
+    
+    // You can stream multiple items to the output, like concatenating
+    cout << "Hello " << fullName << "!" << endl;
+    // endl tells C++ to move to a new line
+    cout << "It is nice to meet you!" << endl;
+    
+    
+    // To read a single word, you can use the cin command
+    cout << "Please enter your first name: ";
+    string name;
+    cin >> name;
+    
+    cout << "Hello " << name << "!" << endl;
+    cout << "It is nice to meet you!" << endl;
+    
+    return 0;
 }
